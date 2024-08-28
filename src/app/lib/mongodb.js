0743aa2cd48f7129ebcdabe4f6cdc4347592f880
Abdulrhman El-Daly <abd911daly@gmail.com>
2024-08-28 09:15:39 +0300

    online mongodb uri

--- src/app/lib/mongodb.js
+++ src/app/lib/mongodb.js
@@ -1,9 +1,11 @@
 const { MongoClient } = require("mongodb");
 
+const uri = process.env.MONGODB_URI;
+
 let mongoClient;
 let clientPromise;
 if (!clientPromise) {
-  mongoClient = new MongoClient("mongodb://localhost:27017");
+  mongoClient = new MongoClient(uri);
   clientPromise = mongoClient.connect();
   console.log("Connected to Mongo");
 }
